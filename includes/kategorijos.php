bbd3c94f440bade6948d9c7614f5bfa70ab74b2b
Kestutis1 <djkestutis@yahoo.com>
2019-05-17 16:42:02 +0300

    pdo prekių išrinkimas

---
+++ includes/kategorijos.php
@@ -0,0 +1,78 @@
+<?php
+
+  class Kat extends Dbh {
+
+      public function kategorijos() {
+        $stmt = $this->connect()->query("SELECT id, kat_pavadinimas, kat_aprašymas FROM
+                      prekiu_kategorijos ORDER BY kat_pavadinimas");
+        // IDEA: Kodo eilutė apačioje negražintų tuščių stulpelių
+        // if ($stmt->fetchColumn() < 0) {
+        if ($stmt->rowCount() < 0) {
+              $parodyk_kategorijas = "<p>Atsiprašome nėra kategorijų peržiūrėti</p><br />";
+        } else {
+          while($cats = $stmt->fetch()) {
+            $cat_id = $cats['id'];
+            $cat_title = strtoupper(stripslashes($cats['kat_pavadinimas']));
+            $cat_desc = stripslashes($cats['kat_aprašymas']);
+            $parodyk_kategorijas = "<p><strong><a href=\"".$_SERVER["PHP_SELF"].
+            "?cat_id=".$cat_id."\">".$cat_title."</a></strong><br />"
+            .$cat_desc."</p><br />";
+            // return $parodyk_kategorijas;
+            echo $parodyk_kategorijas;
+            // echo $cat_title;
+          }
+        }
+      }
+
+      public function getUsersWithCountCheck() {
+        $id = 1;
+        $uid = "JO";
+
+        $stmt =$this->connect()->prepare("SELECT * FROM users WHERE id=? and uid=?");
+        $stmt->execute([$id, $uid]);
+
+        if ($stmt->rowCount()) {
+              while($row = $stmt->fetch()) {
+                  return $row['uid'];
+              }
+        }
+      }
+
+  }
+
+// IDEA: Klasė skirta prekių atvaizdavimui.
+
+class Pre extends Dbh {
+
+  // public $cat_id;
+
+    public function __construct($cat_id) {
+      $this->cat_id = $cat_id;
+    }
+
+    // public function prekes() {
+    //   $id =1;
+    //   $stmt =$this->connect()->prepare("SELECT id, prekės_pavadinimas, prekės_kaina FROM  prekes WHERE kat_id =? ORDER BY prekės_pavadinimas");
+    //   $stmt->execute([$id]);
+    //
+    //   if ($stmt->rowCount()) {
+    //        while ($row = $stmt->fetch()) {
+    //                return $item_id = $row['id'];
+                        // $item_title = stripslashes($items['prekės_pavadinimas']);
+                        // $item_price = $items['prekės_kaina'];
+                        // $display_block .= "<li><a href=\"showitem.php?item_id="
+                        // .$item_id."\">".$item_title."</a></strong>
+                        // (€ ".$item_price.")</li><br />";
+           // }
+    public function prekes() {
+      $stmt =$this->connect()->prepare("SELECT id, prekės_pavadinimas, prekės_kaina FROM  prekes WHERE kat_id =? ORDER BY prekės_pavadinimas");
+      $stmt->execute([$this->cat_id]);
+
+      if ($stmt->rowCount()) {
+           while ($row = $stmt->fetch()) {
+              echo $row['id'];
+
+           }
+    }
+}
+}
--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
 include_once("includes/header.php");
 include_once("includes/dbh.php");
+include("includes/kategorijos.php");
 
     $object = new Dbh;
     $object->connect();
 
+    $display_block = "<h1>Prekių kategorijos</h1>
+    <p>Pasirinkite kategoriją pamatytį jos prekes:</p><br />";
+
+    echo "<section><article><div class = 'flex-container'>
+            <div><h1>Parduotuvės prekių pasirinkimas
+            </h1><br /><div class = 'centre'>$display_block";
+
+   $object = new Kat;
+   $object->kategorijos();
+
+   echo "</div></div></div></article></section>";
+
+
+   if (isset($_GET["cat_id"])) {
+        $cat_id = $_GET["cat_id"];
+
+        $objectas = new Pre($cat_id);
+      echo $objectas->prekes();
+  }
+
 include_once("includes/footer.php");
+
  ?>
