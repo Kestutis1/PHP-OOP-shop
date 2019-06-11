aea72ff2d58ee04e0be796cdd1de2b3ad3983c8c
Kestutis1 <djkestutis@yahoo.com>
2019-06-11 12:29:07 +0300

    krepšelis

--- includes/pasitik_preke.php
+++ includes/pasitik_preke.php
@@ -18,17 +18,40 @@ class pasitik_preke extends Dbh {
          exit();
        }
 }
+
+
    public function pre_i_krepseli() {
      $this->item_id = $_POST["sel_item_id"];
      $this->sesion = $_COOKIE["PHPSESSID"];
      $this->sel_item_qty = $_POST['sel_item_qty'];
-     $this->sel_item_size = $_POST['sel_item_size'];
-     $this->sel_item_color = $_POST['sel_item_color'];
 
+     if (!isset($_POST['sel_item_size']) && !isset($_POST['sel_item_color'])) {
+       $stmt = $this->connect()->prepare("INSERT INTO krepselis (session_id, item_id, sel_item_qty, date_added)
+                                  VALUES (?,?,?, now())");
+       $stmt->execute([$this->sesion, $this->item_id, $this->sel_item_qty]);
+     }
 
-     $stmt = $this->connect()->prepare("INSERT INTO krepselis (session_id, item_id, sel_item_qty, sel_item_size, sel_item_color, date_added)
-                                VALUES (?,?,?,?,?, now())");
-     $stmt->execute([$this->sesion, $this->item_id, $this->sel_item_qty, $this->sel_item_size, $this->sel_item_color]);
+     if (isset($_POST['sel_item_size']) && !isset($_POST['sel_item_color'])) {
+          $this->sel_item_size = $_POST['sel_item_size'];
+          $stmt = $this->connect()->prepare("INSERT INTO krepselis (session_id, item_id, sel_item_qty, sel_item_size, date_added)
+                                     VALUES (?,?,?,?, now())");
+          $stmt->execute([$this->sesion, $this->item_id, $this->sel_item_qty, $this->sel_item_size]);
+     }
+
+      if (!isset($_POST['sel_item_size']) && isset($_POST['sel_item_color'])) {
+          $this->sel_item_color = $_POST['sel_item_color'];
+          $stmt = $this->connect()->prepare("INSERT INTO krepselis (session_id, item_id, sel_item_qty, sel_item_color, date_added)
+                                     VALUES (?,?,?,?, now())");
+          $stmt->execute([$this->sesion, $this->item_id, $this->sel_item_qty, $this->sel_item_color]);
+      }
+
+      if (isset($_POST['sel_item_size']) && isset($_POST['sel_item_color'])) {
+        $this->sel_item_size = $_POST['sel_item_size'];
+        $this->sel_item_color = $_POST['sel_item_color'];
+        $stmt = $this->connect()->prepare("INSERT INTO krepselis (session_id, item_id, sel_item_qty, sel_item_size, sel_item_color, date_added)
+                                   VALUES (?,?,?,?,?, now())");
+        $stmt->execute([$this->sesion, $this->item_id, $this->sel_item_qty, $this->sel_item_size, $this->sel_item_color]);
+      }
   }
 }
  ?>
--- pridėtYkrepšelį.php
+++ pridėtYkrepšelį.php
@@ -8,13 +8,13 @@ if (isset($_POST['sel_item_id'])) {
      $sesion = $_COOKIE["PHPSESSID"];
      $item_id = $_POST["sel_item_id"];
      $sel_item_qty = $_POST['sel_item_qty'];
-     $sel_item_size = $_POST['sel_item_size'];
-     $sel_item_color = $_POST['sel_item_color'];
+     // $sel_item_size = $_POST['sel_item_size'];
+     // $sel_item_color = $_POST['sel_item_color'];
 
      $pasitik_pre_objekt = new pasitik_preke();
      $pasitik_pre_objekt->pre_pavadinimas($item_id);
      $pasitik_pre_objekt->pre_i_krepseli();
-    
+
 }
 
 
